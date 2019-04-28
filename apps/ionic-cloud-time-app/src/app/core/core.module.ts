e49f4f55562fe3e5ab4fafe62f24f6762f7bc160
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2019-04-28 02:25:22 +0200

    `ng g xplat --platforms=ionic --prefix=ctm`, `npm run prepare.ionic.cloud-time-app` (error)

---
+++ apps/ionic-cloud-time-app/src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+// libs
+import { CtmIonicCoreModule } from '@cloudtime/ionic';
+
+@NgModule({
+  imports: [CtmIonicCoreModule]
+})
+export class CoreModule {}
